c9578a4176bc1fb5bda811bd005fc04e16309d64
Riley <ririwilliams.rw@gmail.com>
2024-01-27 11:40:08 -0600

    First commit

---
+++ src/grid.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <raylib.h>
+
+class Grid {
+    public:
+        Grid();
+        void Initialize();
+        void Print();
+        int grid[20][10];
+
+    private:
+        std::vector<Color> GetCellColors();
+        int numRows;
+        int numCols;
+        int cellSize;
+        std::vector<Color> colors;
+};
