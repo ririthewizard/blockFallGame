fb7a1e934ed0e25e068f35ed24dd2648118dd48b
Riley <ririwilliams.rw@gmail.com>
2024-01-29 18:49:54 -0600

    Added score box to hold the actual score #

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include "game.h"
+#include "colors.h"
 
 double lastUpdateTime = 0;
 
@@ -18,7 +19,6 @@ int main()
 {
     InitWindow(500, 620, "raylib BlockFall");
     SetTargetFPS(90);
-    Color darkBlue = {44, 44, 127, 255};
 
     Font font = LoadFontEx("Font/monogram.ttf", 64, 0, 0);
 
@@ -36,7 +36,8 @@ int main()
 
         BeginDrawing();
         ClearBackground(darkBlue);
-        DrawTextEx(font, "Score", {365, 15}, 38, 2, WHITE);
+        DrawTextEx(font, "Score", {360, 15}, 38, 2, WHITE);
+        DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lightBlue);
         game.Draw();
         EndDrawing();
     }
